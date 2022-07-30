93a433c5c97990b7ddc3204711fc1b23c57dd7f6
Vkirovpz <Vkirovpz@students.softuni.bg>
2022-07-30 14:22:59 +0300

    Create User model

---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_rpg2.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; } = string.Empty;
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
+    }
+}
