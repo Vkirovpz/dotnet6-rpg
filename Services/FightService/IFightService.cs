bdef0edc40d6aaf6b072077fead0e48102771c46
Vkirovpz <Vkirovpz@students.softuni.bg>
2022-08-18 11:17:26 +0300

    Create and implement FightMethod

--- Services/FightService/IFightService.cs
+++ Services/FightService/IFightService.cs
@@ -11,5 +11,7 @@ namespace dotnet_rpg2.Services.FightService
         Task<ServiceResponse<AttackResultDto>> WeaponAttack(WeaponAttackDto request);
 
         Task<ServiceResponse<AttackResultDto>> SkillAttack(SkillAttackDto request);
+
+        Task<ServiceResponse<FightResultDto>> Fight(FightRequestDto request);
     }
 }
